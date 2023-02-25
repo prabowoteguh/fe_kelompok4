3538d084b5dc0003099d55c4a0193fb5ddb4409f
yogaseptiann <122095741+yogaseptiann@users.noreply.github.com>
2023-02-25 22:12:00 +0700

    Update histori.js

--- assets/js/histori.js
+++ assets/js/histori.js
@@ -1,59 +1,62 @@
 const API_HISTORI = "https://api-kelompok4.prabowolabs.com/api/dataHistories";
-
+const datas = response.body;
 const getdataHistories = () => {
+
   axios.get(API_HISTORI)
-    .then((result) => {
+    .then((response) => {
+
       const tableBody = document.querySelector('#tablehistory tbody');
       let angkaMulai = 0;
-      result.forEach((item) => { 
+
+      datas.forEach((item) => { 
         var newRow = document.createElement('tr');
 
-        var column1 = document.createElement('td');
+        var column1 = document.createElement('th');
         column1.textContent = angkaMulai += 1;
         newRow.appendChild(column1);
 
-        var column2 = document.createElement('td');
-        column2.textContent = item.id;
+        var column2 = document.createElement('th');
+        column2.textContent = item.amount;
         newRow.appendChild(column2);
 
-        var column3 = document.createElement('td');
-        column3.textContent = item.item_id;
+        var column3 = document.createElement('th');
+        column3.textContent = item.date;
         newRow.appendChild(column3);
 
-        var column4 = document.createElement('td');
-        column4.textContent = item.amount;
+        var column4 = document.createElement('th');
+        column4.textContent = item.description;
         newRow.appendChild(column4);
 
-        var column5 = document.createElement('td');
-        column5.textContent = item.type;
+        var column5 = document.createElement('th');
+        column5.textContent = item.id;
         newRow.appendChild(column5);
 
-        var column6 = document.createElement('td');
-        column6.textContent = item.date;
+        var column6 = document.createElement('th');
+        column6.textContent = item.image;
         newRow.appendChild(column6);
 
-        var colum7 = document.createElement('td');
+        var colum7 = document.createElement('th');
         colum7.textContent = item.invoice_number;
         newRow.appendChild(colum7);
 
-        var colum8 = document.createElement('td');
-        colum8.textContent = item.name;
+        var colum8 = document.createElement('th');
+        colum8.textContent = item.item_id;
         newRow.appendChild(colum8);
 
-        var colum9 = document.createElement('td');
-        colum9.textContent = item.stock;
+        var colum9 = document.createElement('th');
+        colum9.textContent = item.name;
         newRow.appendChild(colum9);
 
-        var colum10 = document.createElement('td');
-        colum10.textContent = item.image;
+        var colum10 = document.createElement('th');
+        colum10.textContent = item.price;
         newRow.appendChild(colum10);
 
-        var colum11 = document.createElement('td');
-        colum11.textContent = item.price;
+        var colum11 = document.createElement('th');
+        colum11.textContent = item.stock;
         newRow.appendChild(colum11);
 
-        var colum12 = document.createElement('td');
-        colum12.textContent = item.description;
+        var colum12 = document.createElement('th');
+        colum12.textContent = item.type;
         newRow.appendChild(colum12);
 
         tableBody.appendChild(newRow);
